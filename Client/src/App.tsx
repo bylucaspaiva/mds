f4c18ca4e6e9782342b99fc434ffdbad8c36ac9a
bylucaspaiva <lucasp4iva@gmail.com>
2023-10-02 15:06:12 -0300

    feat: add shadcn components

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,13 @@
+import { ThemeProvider } from "./@/components/theme-provider";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <nav className="container h-14 flex">
+      <h1 className="text-3xl font-bold underline">MDS Reformas</h1>
+      <h1>teste</h1>
+
+    </nav>
+</ThemeProvider>
 }
 
 export default App;
