c5953c5b23c60815eeba996c5c07d025fdecaa35
bylucaspaiva <lucasp4iva@gmail.com>
2023-10-09 17:10:44 -0300

    feat: add form validation with zod

--- Client/src/@/components/dialog-form.tsx
+++ Client/src/@/components/dialog-form.tsx
@@ -1,26 +1,59 @@
-import { Label } from "@radix-ui/react-label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { Textarea } from "./ui/textarea";
 import { Dialog, DialogContent, DialogTrigger } from "@radix-ui/react-dialog";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "./ui/card";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Nome precisa ter no mínimo duas letras.",
+    })
+    .max(50),
+  email: z.string().min(8, {
+    message: "Email precisa ter no mínimo duas letras.",
+
+  }).max(100)
+});
 
 const DialogForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-          <Button className="h-10">
-             <h1 className="hidden sm:block">Faça um orçamento</h1> 
-             <Icon icon="ic:sharp-message" className="sm:ml-2" width={24} /> 
-          </Button>
+        <Button className="h-10">
+          <h1 className="hidden sm:block">Faça um orçamento</h1>
+          <Icon icon="ic:sharp-message" className="sm:ml-2" width={24} />
+        </Button>
       </DialogTrigger>
 
       <DialogContent>
@@ -30,7 +63,29 @@ const DialogForm = () => {
             <CardDescription>É sem compromisso 😅</CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nome" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              </form>
+              
+              <Button type="submit">Enviar</Button>
+            </Form>
+            {/* <form>
               <div className="px-4 md:px-0 w-full items-center gap-4">
                 <div>
                   <Label htmlFor="name">Nome</Label>
@@ -49,11 +104,11 @@ const DialogForm = () => {
                   <Textarea id="message" placeholder="Descrição do orçamento" />
                 </div>
               </div>
-            </form>
+            </form> */}
           </CardContent>
-          <CardFooter className="flex justify-between mt-2 sm:md-4">
+          {/* <CardFooter className="flex justify-between mt-2 sm:md-4">
             <Button className="ml-auto">Enviar</Button>
-          </CardFooter>
+          </CardFooter> */}
         </Card>
       </DialogContent>
     </Dialog>
